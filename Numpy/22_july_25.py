2fe8925ea1a2bbd37f0e5d5950b080e28f09610f
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-07-27 23:33:10 +0530

    🔁 So in short:
    | Property        | What it means                  |
    | -------------- | ------------------------------------ |
    | `arr.nbytes`   | ✅ Total memory used (in bytes)  |
    | `arr.size`        | ✅ Total number of elements       |
    | `arr.itemsize` | ✅ Size (in bytes) of 1 element    |
    | `arr.nbytes()` | ❌ Invalid — not a method         |

--- Numpy/22_july_25.py
+++ Numpy/22_july_25.py
@@ -4,3 +4,8 @@ np.ones(5)
 # Check Total Memory Used
 arr = np.ones(5)
 print("Total Memory in bytes :",arr.nbytes)
+arr = np.ones(5)  # dtype = float64 by default
+print("Array:", arr)
+print("Item size:", arr.itemsize)   # 8 bytes per float
+print("Total elements:", arr.size)  # 5 elements
+print("Total Memory (nbytes):", arr.nbytes)  # 5 × 8 = 40 bytes
