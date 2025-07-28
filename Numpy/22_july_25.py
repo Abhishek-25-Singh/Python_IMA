31db799f354fc8270d6129f77bc13a38a9c72e4e
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-07-28 23:38:45 +0530

    Both Having Different Outputs Why???**np.ones(5)****print(np.ones(5))**
                       **np.ones(5)** **print(np.ones(5))**
    Both Having Different Outputs Why???
    >>> np.ones(5)  ---> array([1., 1., 1., 1., 1.])
    >>> print(np.ones(5))  -----> [1. 1. 1. 1. 1.]
    ✅ Case 1: When you write just np.ones(5)
    >>> np.ones(5)
           array([1., 1., 1., 1., 1.])
    ➡️ This is not using print().
    In Jupyter Notebook, if a line is the last line in a cell, Jupyter will automatically show the value (called "repr" format).
    🧠 What is repr format? ✅ What is repr()?
    repr stands for "representation" — short for representation() function in Python.
    🎯 Use it when you want to see how Python sees the value, not just how a human reads it.
    
    
    🧠 Simple Rule:
    Function	                Purpose	Example                      Output
    print()	          Shows value for humans	             hello
    repr()	 Shows value for the programmer    'hello' (with quotes)
    
    
    
    ✅ Summary Table
    What	         Function	             Shows
    repr()     Programmer format	     Type + structure
    print()	        User format	            Just the value

--- Numpy/22_july_25.py
+++ Numpy/22_july_25.py
@@ -12,3 +12,17 @@ print("Total Memory (nbytes):", arr.nbytes)  # 5 × 8 = 40 bytes
 print(np.ones(5))
 print(np.ones(5,dtype=int))
 np.ones((5,), dtype=int)
+#🔤 1. With Strings
+msg = "hello"
+print("Value for humans :    ",msg)
+print("Value for the python:",repr(msg))
+
+#🔢 2. With Numbers
+num = 42
+print(num)       # Output: 42 ➡️ Same output here — because number doesn't need special formatting.
+print(repr(num)) # Output: 42 ➡️ Same output here — because number doesn't need special formatting.
+
+# 🧮 3. With a NumPy Array
+arr = np.ones(3)
+print("arr Using Print function :",arr)
+print("Using Representation Function :",repr(arr)) #➡️ repr() tells you it's a NumPy array, not just a list of numbers.
