64af875aafb597737949b3c1e20297666a9480f3
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-08-06 03:58:15 +0530

    Creating List
    1.Empty List
    2.Integer
    3.String

---
+++ 9_july_25_List.py
@@ -0,0 +1,16 @@
+# List are Collection of Elements Which Holds any sort of Data into Square [] Bracket.
+# Creating List
+#Empty List
+empty_list = []
+print("Empty List :",empty_list)
+print("Type of empty_list :",type(empty_list))
+
+#Integer
+int_list = [1,2,3,4,5]
+print("int_list :",int_list)
+print("Type of int_list :",type(int_list))
+
+#String
+Str_list = ["Apple","Banana","Cherry"]
+print("Str_list :",Str_list)
+print("Type of Str_list :",type(Str_list))
