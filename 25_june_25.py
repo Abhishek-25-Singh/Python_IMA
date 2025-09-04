ee9961fee3e9f4709f72aad800f5debe922c716b
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-09-04 23:49:41 +0530

    ✅ Rules for Naming Variables in Python
    1. Must Start with a letter or Underscores(_).
    2.Cannot start with a number.
    3. Can only contain letters, numbers, and underscores.
    4. Case-Sensitive 👉 All three are different variables.

--- 25_june_25.py
+++ 25_june_25.py
@@ -3,3 +3,27 @@
 x = 10      # x stores the number 10
 name = "Abhishek"   # name stores a string
 is_active = True    # stores a boolean (True/False)
+# ✅ Rules for Naming Variables in Python
+# 1. Must Start with a letter or Underscores(_)
+name = "Abhishek"
+_age = 23
+# 1num = 1 # SyntaxError: invalid decimal literal
+# $name = "abhishek" # SyntaxError: invalid syntax
+# 1name = "Abhishek" # SyntaxError: invalid decimal literal
+
+# 2.Cannot start with a number
+num1 = 1
+# 1num = 1 # SyntaxError: invalid decimal literal
+name = "abhishek"
+# 1name = "Abhishek" # SyntaxError: invalid decimal literal
+
+# 3. Can only contain letters, numbers, and underscores
+user_id = 10
+# user-id = 10 # SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+score2 = 99
+# score 2 = 99 # SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+
+#.4 Case-Sensitive 👉 All three are different variables.
+age = 20
+Age = 25
+AGE = 30
