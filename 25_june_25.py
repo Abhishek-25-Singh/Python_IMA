4c1cbb34a7a59b2ee028dd7d041997fd163a1944
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-09-04 23:47:36 +0530

    Variable

--- 25_june_25.py
+++ 25_june_25.py
@@ -1,1 +1,5 @@
 # 25_june_25 <------------------------> 02_Sep_25 8:00pm(practice)
+# Store and print Variables
+x = 10      # x stores the number 10
+name = "Abhishek"   # name stores a string
+is_active = True    # stores a boolean (True/False)
