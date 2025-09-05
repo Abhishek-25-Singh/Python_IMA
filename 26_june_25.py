d80cf15b0630866a541ca5ea5104912b5313eaf0
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-09-05 22:02:26 +0530

    ✅  Python dose The inferencing Automatically
    Type inferencing ---> Python Automatically Decide That a and b are float Because They have Decimal points,so c also becomes a Float.
    Type inferencing ---> Python Automatically guesses/figure out The data type of a variable from The value assign to it,without you having to explicitly declare it.

--- 26_june_25.py
+++ 26_june_25.py
@@ -1,1 +1,9 @@
+# # 26_june_25 <------------------> 05_09_25 4:20pm
 
+# Python dose The inferencing Automatically.
+a = 10.226  # float
+b = 5.229  # Float
+c = a + b  # result(c) also be float because both operands (a,b) are float
+print("a + b = ",c)
+print("Type of c :",type(c))
+print("Round od The Value upto 2 Decimal :",round(c,2))
