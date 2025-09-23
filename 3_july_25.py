064051435d66af0531eb1ff8c4d8fb5eb212cb95
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-09-23 23:51:04 +0530

    If Statement

--- 3_july_25.py
+++ 3_july_25.py
@@ -1,1 +1,8 @@
 # 🔹 Conditional Statements in Python
+
+# 1) If Statement - 
+# If will only Execute if the given condition is true
+
+age = 20
+if age >= 18:
+    print("You are eligible to vote! " )
