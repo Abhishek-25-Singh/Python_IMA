365da8ff35413cd89eba749d80120c8f86ca63e5
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-09-23 23:52:11 +0530

    If-Else Statement
    Executes one block if condition is True, otherwise another block.

--- 3_july_25.py
+++ 3_july_25.py
@@ -11,3 +11,20 @@ if age >= 18:
 age = int(input("Enter your age in Numbers : "))
 if age >= 18:
           print(age,"Is ELigible to vote ")
+
+
+# 2) If-Else Statement
+#    Executes one block if condition is True, otherwise another block.
+
+age = int(input("Enter your age in Numbers : "))
+if age >= 18:
+          print(age,"Is ELigible to vote ")
+else:
+    print(age," Not eligible to Vote")
+
+
+marks = 45
+if marks >= 40:
+    print("You passed.")
+else:
+    print("You failed.")
