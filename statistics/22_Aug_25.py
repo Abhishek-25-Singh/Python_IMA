d097c7bfab78d6051cfb3379e0c0c5a59baa8cc5
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-10-04 23:35:43 +0530

    🔑 Key Points About Skewness (before Python use)
    1. Meaning of Skewness
    2. Type of Data Required
    3. Sample Size
    4. Outliers Effect
    5.Graph vs. Formula
    6.Python Functions

--- statistics/22_Aug_25.py
+++ statistics/22_Aug_25.py
@@ -2,3 +2,5 @@
 # Skewness is a statistical measure that describes the symmetry or asymmetry in the distribution of data.
 # If a dataset follows a Normal Distribution (bell-shaped curve), it is perfectly symmetrical, and skewness is 0.
 # When the data deviates from normality, skewness measures the degree and direction of asymmetry.
+
+#🔑 Key Points About Skewness (before Python use)
