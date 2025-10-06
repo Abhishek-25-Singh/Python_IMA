38d1fb8ceca29594b6177a88edbd058b43a50633
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2025-10-06 23:52:25 +0530

    range(start,stop,step/skip)

--- 4_july_25.py
+++ 4_july_25.py
@@ -1,3 +1,12 @@
 # 🧠 Syntax of range() : **range(start, stop, step/skip)**
 number = list(range(11)) # it will Take default From 0
 print(number)
+print(list(range(5)))
+for i in range(5):
+    print(i)
+
+
+range(2,6)
+
+for i in range(2,6):
+    print(i)
